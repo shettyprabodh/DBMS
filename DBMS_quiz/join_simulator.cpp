346e25f7cfd5cc7a325c13a3cf82db0594dd6b16
shettyprabodh <shettyprabodh@gmail.com>
2016-04-04 14:20:11 +0530

    resulved some errors.

--- DBMS_quiz/join_simulator.cpp
+++ DBMS_quiz/join_simulator.cpp
@@ -4,6 +4,30 @@ using namespace std;
 
 int rec_sz_1, rec_sz_2, page_size, no_of_pages, max_no_of_rounds,no_of_buckets;
 int round_prime;
+int size_rel_1, size_rel_2;
+
+
+int number_of_records(char *file)
+{
+    int line_no;
+    ifstream fileptr;
+    string line;
+
+    fileptr.open(file);
+    line_no = 0;
+
+    while(getline(fileptr,line))
+    {
+    if(line.compare("=================================================") == 0)
+        continue;
+
+    if(line.compare("#_of_pages") == 0)
+        break;
+
+    line_no++;
+    }
+    return line_no;
+}
 
 int hash_func(int x)
 {
@@ -30,20 +54,9 @@ void partition(char *file, int rec_size)
 
     vector < vector < int > > bucket(no_of_buckets);
     vector < int > no_pages_bucket(no_of_buckets,0);
-    //vector < int > row;
-
-    // for(int i=0; i < no_of_buckets; i++)
-    // {
-    //     row.push_back(i);
-    //     bucket.push_back(row);
-    //     row.clear();
-    // }
 
     cout << "Hashing Round " << round_prime << "\n";
     cout << "Reading " << file << "\n";
-
-    //assert(fileptr1.is_open());
-
 
     for(int i=0; i < no_of_buckets; i++ )
     {
@@ -126,9 +139,10 @@ void partition(char *file, int rec_size)
         //flushing_file << "+++++++++++++++++++++++++++++++++++++++++++++++++\n";
         flushing_file << "#_of_pages\n";
         flushing_file << no_pages_bucket[i] << "\n";
+        cout << filename_gen(string(file), i) << " : " << no_pages_bucket[i] << " pages.\n" ;
     }
 
-    cout << "Done with " << file << ".\n";
+    cout << "Done with " << file << ".\n\n\n";
 }
 
 void join_anyway(char *file1, char *file2)
@@ -141,6 +155,7 @@ void join_anyway(char *file1, char *file2)
 
     fileptr1.open(file1);
     resultant_table.open("resultant_table.txt", ios::app);
+    cout << "Matching pairs are\n";
 
     while(getline(fileptr1,line1))
     {
@@ -148,7 +163,7 @@ void join_anyway(char *file1, char *file2)
             continue;
         if(line1.compare("#_of_pages") == 0)
         {
-            cout << "Bucket: " << ".No further processing required.\n";
+            cout << "Bucket: " << ".No further processing required from " << file1 << "\n";
             break;
         }
 
@@ -165,15 +180,19 @@ void join_anyway(char *file1, char *file2)
 
 
             if(line1.compare(line2) == 0)
+            {
+                cout << line1 << " : " << line2 << "\n";
                 resultant_table << line1 << "\n";
+            }
         }
         fileptr2.close();
     }
 
     resultant_table.close();
     fileptr1.close();
-    if(round_prime>0)
-        round_prime=0;
+    // if(round_prime>0)
+    //     round_prime--;
+    cout << "\n\n";
 
 }
 
@@ -209,8 +228,12 @@ void join(char *file1, char *file2)
         fileptr1.close();
         fileptr2.close();
 
+        cout << "Bucket " << i << ": Total size is " << (no_pages_bucket_1 + no_pages_bucket_2) << " pages. \n";
+
         if(no_pages_bucket_1 + no_pages_bucket_2 >= no_of_pages)
         {
+            cout << "Cannot perform in memory join.\n";
+
             if(round_prime >= max_no_of_rounds)
             {
                 file_nxt_round_1 = filename_gen(string(file1),i);
@@ -221,6 +244,27 @@ void join(char *file1, char *file2)
             }
             else
             {
+                int records_1, records_2;
+
+                records_1 = number_of_records(file1);
+                records_2 = number_of_records(file2);
+
+                if((records_1*size_rel_1)/page_size == 0)
+                    size_rel_1 = (records_1*rec_sz_1)/page_size;
+                else
+                    size_rel_1 = (records_1*rec_sz_1)/page_size + 1;
+
+                if((records_2*size_rel_2)/page_size == 0)
+                    size_rel_2 = (records_2*rec_sz_2)/page_size;
+                else
+                    size_rel_2 = (records_2*rec_sz_2)/page_size + 1;
+
+                cout << "Performing next round of hashing for bucket " << i << "\n";
+                cout << "Size of relation 1: " << size_rel_1 << "\n";
+                cout << "Size of relation 2: " << size_rel_2 << "\n";
+                cout << "Total number of available pages: " << no_of_pages << "\n";
+                cout << "Number of buckets in hash table: " << no_of_pages - 1 << "\n\n\n";
+
                 file_nxt_round_1 = filename_gen(string(file1),i);
                 file_nxt_round_2 = filename_gen(string(file2),i);
                 round_prime = round_prime + 1;
@@ -229,15 +273,15 @@ void join(char *file1, char *file2)
                 partition((char *)file_nxt_round_2.c_str(),rec_sz_2);
                 join((char *)file_nxt_round_1.c_str(), (char *)file_nxt_round_2.c_str());
                 if(round_prime>0)
-                    round_prime = 0;
+                    round_prime--;
             }
         }
         else
         {
-            cout << "Bucket " << i << ": Total size is " << (no_pages_bucket_1 + no_pages_bucket_2) << " pages\n";
+            //cout << "Bucket " << i << ": Total size is " << (no_pages_bucket_1 + no_pages_bucket_2) << " pages\n";
             cout << "Total available pages " << no_of_pages << "\n";
             cout << "Performing in memory join\n";
-
+            cout << "Matching pairs are\n";
             fileptr1.open(filename_gen(string(file1),i));
             resultant_table.open("resultant_table.txt", ios::app);
 
@@ -248,7 +292,7 @@ void join(char *file1, char *file2)
                     continue;
                 if(line1.compare("#_of_pages") == 0)
                 {
-                    cout << "Bucket: " << i << ".No further processing required.\n";
+                    cout << "Bucket: " << i << ". No further processing required from " << file1 << "\n";
                     break;
                 }
 
@@ -263,19 +307,22 @@ void join(char *file1, char *file2)
                         break;
                     }
 
-
                     if(line1.compare(line2) == 0)
+                    {
+                        cout << line1 << " : " << line2 << "\n";
                         resultant_table << line1 << "\n";
+                    }
                 }
                 fileptr2.close();
             }
 
             fileptr1.close();
             resultant_table.close();
-            if(round_prime>0)
-            {
-                round_prime = 0;
-            }
+            // if(round_prime>0)
+            // {
+            //     round_prime--;
+            // }
+            cout << "\n\n\n";
         }
     }
 }
@@ -284,21 +331,17 @@ void join(char *file1, char *file2)
 int main(int argc, char *argv[])
 {
     char *file_1, *file_2;
-    int size_rel_1, size_rel_2;
-
+    int records_1, records_2;
     string line1, line2;
     char *line_1, *line_2;
     line_1 = (char *)malloc(100*sizeof(char));
     line_2 = (char *)malloc(100*sizeof(char));
-    ifstream fileptr_1 (file_1);
-    ifstream fileptr_2 (file_2);
-    ofstream resultant_table;
 
-    cout << argc << "\n";
-    for(int i=0; i < argc; i++)
-    {
-        cout << argv[i] << "\n";
-    }
+    //cout << argc << "\n";
+    // for(int i=0; i < argc; i++)
+    // {
+    //     cout << argv[i] << "\n";
+    // }
 
     file_1 = argv[1];
     file_2 = argv[2];
@@ -310,40 +353,25 @@ int main(int argc, char *argv[])
     round_prime = 0;
     no_of_buckets = no_of_pages - 1;
 
+    records_1 = number_of_records(file_1);
+    records_2 = number_of_records(file_2);
 
-    fileptr_1.open(file_1);
-    //fileptr_2.open(file_2);
-    resultant_table.open("resultant_table.txt");
-
-
-    if(page_size%rec_sz_1 == 0)
-        size_rel_1 = page_size/rec_sz_1;
+    if((records_1*size_rel_1)/page_size == 0)
+        size_rel_1 = (records_1*rec_sz_1)/page_size;
     else
-        size_rel_1 = page_size/rec_sz_1 + 1;
+        size_rel_1 = (records_1*rec_sz_1)/page_size + 1;
 
-    if(page_size%rec_sz_2 == 0)
-        size_rel_2 = page_size/rec_sz_2;
+    if((records_2*size_rel_2)/page_size == 0)
+        size_rel_2 = (records_2*rec_sz_2)/page_size;
     else
-        size_rel_2 = page_size/rec_sz_2 + 1;
+        size_rel_2 = (records_2*rec_sz_2)/page_size + 1;
+
 
     cout << "Size of relation 1: " << size_rel_1 << "\n";
     cout << "Size of relation 2: " << size_rel_2 << "\n";
     cout << "Total number of available pages: " << no_of_pages << "\n";
     cout << "Number of buckets in hash table: " << no_of_pages - 1 << "\n";
-
-
-    while(std::getline(fileptr_1,line1))
-    {
-        //cout << "Hello\n";
-        fileptr_2.open(file_2);
-        while(std::getline(fileptr_2,line2))
-        {
-            if(line1 == line2)
-                resultant_table << line1 << "\n";
-        }
-        fileptr_2.close();
-    }
-    fileptr_1.close();
+    cout << "\n\n";
 
     partition(file_1,rec_sz_1);
     partition(file_2,rec_sz_2);
